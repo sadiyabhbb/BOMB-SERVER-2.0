060ce0def53c70a2e2743e1f60a0b3d6b7ada95b
sadiyabhbb <sadiyabhbb@proton.me>
2025-09-03 21:18:41 +0600

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const sendOtp = require('./src/osudpotro');
-const getApiInfo = require('./src/info');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
